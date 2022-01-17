3c57f2577f3160deca7b473a7179a631107500fc
recktg095 <lucas.magaldi@hotmail.com>
2022-01-17 20:42:11 -0300

    Assets folder

--- ui-site/src/Components/Navbar/index.jsx
+++ ui-site/src/Components/Navbar/index.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import './styles.css'
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+
+import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src = {logo} alt="logo"/>
+                </div>
+                <div className="gpt3_navbar-links-container">
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#home">What is GPT3 ?</a></p>
+                    <p><a href="#home">Open AI</a></p>
+                    <p><a href="#home">Case Studios</a></p>
+                    <p><a href="#home">Library</a></p>
+                </div>
+            </div>
+        </div>
     )
 }
 
